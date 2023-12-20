e7c3d66389ebc45d317c67236e4ddf407809caf6
Home <home@MacBook-Pro-Home.local>
2023-12-20 13:33:51 +0200

    commit 4

--- script.js
+++ script.js
@@ -2,19 +2,25 @@ import fetch from "node-fetch";
 
 
 const pricParm = process.argv.slice(2)
-let cycleKey = "SCI_R4444"
+let cycleKey = "SSS-1111,SSS-2222"
 
 if ( pricParm[0] != undefined ){
     // console.log(pricParm[0])
     cycleKey = pricParm[0].split('=')[1]
 }
 
+let cycleKeys = cycleKey.split(",")
+let text = ""
+for (let i in cycleKeys){
+    text = text + `${cycleKeys[i]}\n`
+}
 
 console.log("=================")
 console.log("====         ====")
 console.log("=               =")
 console.log("=               =")
-console.log(`~~~ ${cycleKey} ~~~`)
+console.log(" ")
+console.log(`${text}`)
 console.log("=               =")
 console.log("=               =")
 console.log("====         ====")
