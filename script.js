7aa82df669d6caf85d5b9789be20e31031379281
Home <home@MacBook-Pro-Home.local>
2023-12-20 14:02:42 +0200

    commit 2

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ import fetch from "node-fetch";
 const pricParm = process.argv.slice(2)
 let cycleKey = "SSS-1111,SSS-2222"
 
+const token = process.env.ACCESS_TOKEN
+
 if ( pricParm[0] != undefined ){
     // console.log(pricParm[0])
     cycleKey = pricParm[0].split('=')[1]
@@ -13,7 +15,7 @@ let cycleKeys = cycleKey.split(",")
 console.log(cycleKeys)
 
 for (let i in cycleKeys){
-    
+
     let data = {
         "ref":"master", 
         "inputs": 
@@ -25,7 +27,7 @@ for (let i in cycleKeys){
         const response = await fetch(`https://api.github.com/repos/Spirlhark/cycle_run/actions/workflows/79980694/dispatches`, {
             method: "POST",
             headers: {
-                Authorization: "token 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",
+                Authorization: `token ${token}`,
                 Accept: "application/vnd.github.v3+json"
             },
             body: JSON.stringify(data)
