6a2848f18087a8d584625f794c02fdc3b2a5a83c
Home <home@MacBook-Pro-Home.local>
2024-01-04 20:08:26 +0200

    test COMMit

---
+++ .github/actions/transform/index.js
@@ -0,0 +1,21 @@
+const core = require("@actions/core");
+try {
+  // `cycle_key` input defined in action metadata file with default value "XXX-1111,XXX-2222"
+  const os = core.getInput("cycle_key", { require: true });
+// // `nodeversion` input defined in action metadata file with default value "10,16"
+//   const nodeVersion = core.getInput("nodeversion", { required: true });
+core.info(
+    `Inputs to transform ${JSON.stringify({
+        cycle_key
+    })}`
+  );
+// create arrays by splitting input on ','
+  const cycle_keyArray = cycle_key.split(",");
+// Reduce array to string with single quotation mark
+  const osInputStringified = cycle_keyArray.map(_cycle_key => `'${_cycle_key}'`).toString();
+  core.setOutput("cycle_key", `[${cycle_keyInputStringified}]`);
+} catch (error) {
+  core.setFailed(
+    `Action failed to transform input with error: ${error.message}`
+  );
+}
