eeb8e29015735d6a633a9904323d12d6bd4098d5
Home <home@MacBook-Pro-Home.local>
2024-05-08 20:59:15 +0300

    get data with s3 8

--- .github/actions/transform/index.js
+++ .github/actions/transform/index.js
@@ -4,44 +4,49 @@ import fetch from "node-fetch";
 
 async function executionData() {
 
-    const response = await fetch(`https://dimacyclebuket.s3.amazonaws.com/CycleList.json`, {
-          method: "GET", // or 'PUT' or 'POST'
-          headers: {},
-          });
-      
-    const res = await response.json();
-    console.log("++++++++")
-    console.log(res.key);
-    console.log("++++++++")
+  let currentDate = new Date();
+  console.log("---===***===---")
+  console.log(currentDate)
+  console.log("---===***===---")
 
-    let cycleKeys=res.key
+  const response = await fetch(`https://dimacyclebuket.s3.amazonaws.com/CycleList.json`, {
+        method: "GET", // or 'PUT' or 'POST'
+        headers: {},
+        });
+    
+  const res = await response.json();
+  console.log("++++++++")
+  console.log(res.key);
+  console.log("++++++++")
 
-    try {
-      const cycle_key = cycleKeys.toUpperCase()
-      console.log(cycle_key)
-    // // `nodeversion` input defined in action metadata file with default value "10,16"
-    //   const nodeVersion = core.getInput("nodeversion", { required: true });
-    core.info(
-        `Inputs to transform ${JSON.stringify({
-            cycle_key
-        })}`
-      );
-    // create arrays by splitting input on ','
-      const cycle_keyArray = cycle_key.split(",");
-      console.log("=======")
-      console.log(cycle_keyArray)
-      console.log("=======")
-    // Reduce array to string with single quotation mark
-      const cycle_keyInputStringified = cycle_keyArray.map(_cycle_key => `'${_cycle_key}'`).toString();
-      core.setOutput("cycle_key", `[${cycle_keyInputStringified}]`);
-      console.log("-------")
-      console.log(cycle_keyInputStringified)
-      console.log("-------")
-    } catch (error) {
-      core.setFailed(
-        `Action failed to transform input with error: ${error.message}`
-      );
-    }
+  let cycleKeys=res.key
+
+  try {
+    const cycle_key = cycleKeys.toUpperCase()
+    console.log(cycle_key)
+  // // `nodeversion` input defined in action metadata file with default value "10,16"
+  //   const nodeVersion = core.getInput("nodeversion", { required: true });
+  core.info(
+      `Inputs to transform ${JSON.stringify({
+          cycle_key
+      })}`
+    );
+  // create arrays by splitting input on ','
+    const cycle_keyArray = cycle_key.split(",");
+    console.log("=======")
+    console.log(cycle_keyArray)
+    console.log("=======")
+  // Reduce array to string with single quotation mark
+    const cycle_keyInputStringified = cycle_keyArray.map(_cycle_key => `'${_cycle_key}'`).toString();
+    core.setOutput("cycle_key", `[${cycle_keyInputStringified}]`);
+    console.log("-------")
+    console.log(cycle_keyInputStringified)
+    console.log("-------")
+  } catch (error) {
+    core.setFailed(
+      `Action failed to transform input with error: ${error.message}`
+    );
+  }
 
 }
 
