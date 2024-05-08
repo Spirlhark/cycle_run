4e3c09a5fcadf414d17d9c50618e140ceb383edd
Home <home@MacBook-Pro-Home.local>
2024-05-08 20:51:35 +0300

    get data with s3 7

--- .github/actions/transform/index.js
+++ .github/actions/transform/index.js
@@ -2,8 +2,6 @@
 import core from "@actions/core";
 import fetch from "node-fetch";
 
-
-let cycleKeys;
 async function executionData() {
 
     const response = await fetch(`https://dimacyclebuket.s3.amazonaws.com/CycleList.json`, {
@@ -16,17 +14,10 @@ async function executionData() {
     console.log(res.key);
     console.log("++++++++")
 
-    cycleKeys=res.key
-    // console.log("=======")
-    // console.log(cycleKeys)
-    // console.log("=======")
+    let cycleKeys=res.key
 
     try {
-      // `cycle_key` input defined in action metadata file with default value "XXX-1111,XXX-2222"
-      // let cycle_keyUp = core.getInput("cycle_key", { require: true });
-      let cycle_keyUp = cycleKeys
-
-      const cycle_key = cycle_keyUp.toUpperCase()
+      const cycle_key = cycleKeys.toUpperCase()
       console.log(cycle_key)
     // // `nodeversion` input defined in action metadata file with default value "10,16"
     //   const nodeVersion = core.getInput("nodeversion", { required: true });
@@ -55,36 +46,3 @@ async function executionData() {
 }
 
 executionData()
-
-console.log("---===***====---")
-console.log(cycleKeys)
-console.log("---===***====---")
-
-// try {
-//   // `cycle_key` input defined in action metadata file with default value "XXX-1111,XXX-2222"
-//   let cycle_keyUp = core.getInput("cycle_key", { require: true });
-//   const cycle_key = cycle_keyUp.toUpperCase()
-//   console.log(cycle_key)
-// // // `nodeversion` input defined in action metadata file with default value "10,16"
-// //   const nodeVersion = core.getInput("nodeversion", { required: true });
-// core.info(
-//     `Inputs to transform ${JSON.stringify({
-//         cycle_key
-//     })}`
-//   );
-// // create arrays by splitting input on ','
-//   const cycle_keyArray = cycle_key.split(",");
-//   console.log("=======")
-//   console.log(cycle_keyArray)
-//   console.log("=======")
-// // Reduce array to string with single quotation mark
-//   const cycle_keyInputStringified = cycle_keyArray.map(_cycle_key => `'${_cycle_key}'`).toString();
-//   core.setOutput("cycle_key", `[${cycle_keyInputStringified}]`);
-//   console.log("-------")
-//   console.log(cycle_keyInputStringified)
-//   console.log("-------")
-// } catch (error) {
-//   core.setFailed(
-//     `Action failed to transform input with error: ${error.message}`
-//   );
-// }
